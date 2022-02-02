5b1ac09406eb138fbf886c1f3346c4af537ec32b
Yusung <qkrdbtjd15@naver.com>
2022-02-02 17:05:23 +0900

    Update #2 - AdminController
    ModelAndView 타입으로 String과 객체(Map)을 admin/places.html에 전달

--- src/main/java/com/project/getinline/controller/AdminController.java
+++ src/main/java/com/project/getinline/controller/AdminController.java
@@ -1,17 +1,32 @@
 package com.project.getinline.controller;
 
+import com.project.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces(){
-        return "admin/places";
+    public ModelAndView adminPlaces(
+            PlaceType placeType,
+            String placeName,
+            String address
+    ){
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
