7ef66222c6b7ce99b95867e294db1c04ceef56ed
Yusung <qkrdbtjd15@naver.com>
2022-02-08 21:50:02 +0900

    Add #2 - EventRequest, EventResponse

---
+++ src/main/java/com/project/getinline/dto/EventResponse.java
@@ -0,0 +1,48 @@
+package com.project.getinline.dto;
+
+import com.project.getinline.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+
+    public static EventResponse of(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventResponse(placeId, eventName, eventStatus, eventStartDatetime,eventEndDatetime
+        ,currentNumberOfPeople,capacity,memo);
+    }
+
+    public static EventResponse from(EventDTO eventDTO){
+        if(eventDTO == null){
+            return null;
+        }
+
+        return EventResponse.of(
+                eventDTO.placeId(),
+                eventDTO.eventName(),
+                eventDTO.eventStatus(),
+                eventDTO.eventStartDatetime(),
+                eventDTO.eventEndDatetime(),
+                eventDTO.currentNumberOfPeople(),
+                eventDTO.capacity(),
+                eventDTO.memo()
+        );
+    }
+}
