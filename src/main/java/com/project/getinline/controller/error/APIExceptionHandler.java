395f2495ac0883ec291e15eb58f82a02a035196b
Yusung <qkrdbtjd15@naver.com>
2022-02-08 21:51:00 +0900

    Add #2 - APIExceptionHandler, Test
    APIExceptionHandler와 그에 해당하는 테스트 추가

--- src/main/java/com/project/getinline/controller/error/APIExceptionHandler.java
+++ src/main/java/com/project/getinline/controller/error/APIExceptionHandler.java
@@ -6,19 +6,22 @@ import com.project.getinline.exception.GeneralException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice(annotations = RestController.class)
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request){
+
+        return getInternalResponseEntity(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
+    }
 
     // 일반적인 예외 상황 처리
     @ExceptionHandler
@@ -28,26 +31,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
                 HttpStatus.BAD_REQUEST :
                 HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return super.handleExceptionInternal
-                (e,
-                APIErrorResponse.of(false,errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request);
+        return getInternalResponseEntity(e, errorCode, HttpHeaders.EMPTY, status, request);
     }
 
     // general 에서 잡지 못한 예외 처리
     @ExceptionHandler
     public ResponseEntity<Object> exception(Exception e, WebRequest request){
-        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return super.handleExceptionInternal
-                (e,
-                APIErrorResponse.of(false,errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request);
+        return getInternalResponseEntity(e, ErrorCode.INTERNAL_ERROR, HttpHeaders.EMPTY, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     // 스프링에서 발생한 에러 처리(ResponseEntityExceptionHandler 클래스를 상속받아
@@ -58,9 +49,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
                 ErrorCode.SPRING_BAD_REQUEST :
                 ErrorCode.SPRING_INTERNAL_ERROR;
 
-        return super.handleExceptionInternal
-                (ex,
-                APIErrorResponse.of(false,errorCode.getCode(), errorCode.getMessage(ex)),
+        return getInternalResponseEntity(ex, errorCode, headers, status, request);
+
+    }
+
+    private ResponseEntity<Object> getInternalResponseEntity(Exception e, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
                 headers,
                 status,
                 request);
