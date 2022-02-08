58a2f476e677b02d9d0cb19e45972befe371308d
Yusung <qkrdbtjd15@naver.com>
2022-02-08 21:52:20 +0900

    Add #2 - ErrorCodeTest
    에러 코드에 VALIDATION_ERROR 추가
    에러 코드 테스트 추가

--- src/main/java/com/project/getinline/constant/ErrorCode.java
+++ src/main/java/com/project/getinline/constant/ErrorCode.java
@@ -15,9 +15,11 @@ public enum ErrorCode {
 
     BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "bad request"),
     SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
+    VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation Error"),
 
     INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
-    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error")
+    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error"),
+    DATA_ACCESS_ERROR(20002, ErrorCategory.SERVER_SIDE, "Data access error")
     ;
 
     private final Integer code;
@@ -25,7 +27,7 @@ public enum ErrorCode {
     private final String message;
 
     public String getMessage(Exception e){
-        return getMessage(e.getMessage());
+        return getMessage(this.getMessage() + " - " + e.getMessage());
     }
 
     public String getMessage(String message){
